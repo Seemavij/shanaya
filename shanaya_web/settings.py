2446b7b92c553af506ce02e8e0e7923f6aa721e9
Seema Vij <seemavij@live.ie>
2024-07-29 16:27:17 +0000

    remove secret key and set debug

--- shanaya_web/settings.py
+++ shanaya_web/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1psc#z^rs=(ap#-d8_=m==q5+vj1lkgdy-)2ch#s0wdb&g&j-2'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['shanaya.herokuapp.com','localhost'
 ]
