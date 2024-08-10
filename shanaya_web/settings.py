2549bf4ad45051153b1a8c356830e924b51d5075
Seema Vij <seemavij@live.ie>
2024-08-10 18:05:00 +0000

    updated files

--- shanaya_web/settings.py
+++ shanaya_web/settings.py
@@ -74,6 +74,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'shanaya_web.urls'
