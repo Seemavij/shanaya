e02da8f8f3e44ef0ea6dcc12962cbc7f4cf37ab4
Seema Vij <seemavij@live.ie>
2024-08-27 09:32:40 +0000

    correction done for bag folder structure

--- products/views.py
+++ products/views.py
@@ -68,7 +68,7 @@ def product_detail(request, product_id):
         'product': product,
     }
 
-    return render(request, 'products/product_detail.html', context)
+    return render(request, 'product_detail.html', context)
 
 
 @login_required
