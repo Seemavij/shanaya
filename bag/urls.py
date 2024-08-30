eb2400c25beefaf5b5dcb8c739232efd59d36986
Seema Vij <seemavij@live.ie>
2024-08-30 17:28:56 +0000

    deployment

--- bag/urls.py
+++ bag/urls.py
@@ -1,9 +1,12 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
-    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
-    path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
-    path('remove/<item_id>/', views.remove_from_bag, name='remove_from_bag'),
+    path('add/<bag_item_id>/', views.add_to_bag, name='add_to_bag'),
+    path(
+        'remove/<bag_item_id>/', views.remove_from_bag, name='remove_from_bag'
+        ),
 ]
