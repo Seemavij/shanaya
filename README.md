c2439f51eeea559b567b2872beef9dc29be23814
Seema Vij <seemavij@live.ie>
2024-10-14 10:00:02 +0000

    add correction in validation

--- README.md
+++ README.md
@@ -1,16 +1,78 @@
 ![alt image](media/responsive.png)
 
+
 ! Shanaya is a ecommerce site. It sell Bridal dresses, Indo western dresses, Men sherwani & gown online.Variety of collection and clothes for sale on resonable rate.
 
-You can reach the live site here.
+You can reach the live site here
 
 * Table of Contents:
 
 . Business-Model
 
-![alt image](media/about.us.png)
+![alt image](media/about_us.png)
 
-. User-Experience-Design
+. User Experience (UX)
+
+User stories :
+
+All user stories can be seen here Shanaya
+
+* Site User Stories
+
+. As a site user I want to be able to register for an account so that I can have a personal account with my delivery details saved
+
+. As a site user I want to be able to login and logout so that I can access my profile info
+
+. As a site user I want to be able to receive an email confirmation after sign up so that I can verify my account registration was successful
+
+. As a site user I want to be able to have a personalised user profile so that I can view my order history and delivery information
+
+. As a site user I want to be able to leave a product review so that I can advise potential customers with my advise
+
+. As a site user I want to be able to sign up to a newsletter so that I can get the latest news and offers
+
+. As a **site user ** I want to be able to **unsubscribe for the newsletter ** so that I can stop receiving emails
+
+. As a site user I can return to Home after http 404 or 500 response so that I feel I am still working within the website and can navigate easily
+
+* Shopper User Stories
+
+. As a shopper I want to be able to view my total cost so that I can I don't spend too much
+
+. As a shopper I want to be able to sort any list of products so that I can easily sort by price, best rated, etc
+
+. As a shopper I want to be able to sort products into categories so that I can split products into categories to make it easier to find what I want
+
+. As a shopper I want to be able to search a product by name so that I can easily find the product I am searching for
+
+. As a shopper I want to be able to select a specific size so that I can I can make sure the product will suit my needs
+
+. As a shopper I want to be able to view the items in my bag so that I can check out my bag contents and know exactly what I am buying
+
+. As a shopper I want to be able to adjust the quantity of items in my bag so that I can easily make changes
+
+. As a shopper I want to be able to enter my card details so that I can make a purchase
+
+. As a shopper I want to be able to view my order after I complete a purchase so that I can verify I have not made any mistakes
+
+. As a shopper I want to be able to receive an email confirmation after making a purchase so that I can keep the confirmation for any future needs
+
+. As a shopper I want to be able to contact Shanaya so that I can get in touch regarding a product/ issue, etc
+
+Admin Stories
+
+As an admin I want to be able to add a product so that I can I can add new products my store
+
+As an admin I want to be able to edit a product so that I can make changes to any product
+
+As an admin I want to be able to delete a product so that I can remove an item that is no longer for sale
+
+As an admin I want to be able to view all messages from the contact form so that I can communicate with customers in a timely manner
+
+As an admin I want to be able to view all messages from the contact form so that I can communicate with customers in a timely manner
+
+As an admin I want to be able to view a list of subscribers in my admin so that I can see the amount of subscribers we have
+
 
 . Site Goals
 
@@ -52,6 +114,7 @@ You can reach the live site here.
 ![alt image](media/checkout_models.py.png)
 
 . Order Confirmation
+
 ![alt image](media/order_confirmation.png)
 
 . Profile Page
@@ -704,7 +767,7 @@ tag that was used incorrectly inside a .
 
 . Select create new app
 
-. Enter app name (kids-bored)
+. Enter app name (Shanaya)
 
 . Select region and click create app (europe)
 
--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,8 +6,8 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripePublicKey = £('#id_stripe_public_key').text().slice(1, -1);
-var clientSecret = £('#id_client_secret').text().slice(1, -1);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
@@ -38,7 +38,7 @@ card.addEventListener('change', function (event) {
             </span>
             <span>£{event.error.message}</span>
         `;
-        £(errorDiv).html(html);
+        $(errorDiv).html(html);
     } else {
         errorDiv.textContent = '';
     }
@@ -50,9 +50,9 @@ var form = document.getElementById('payment-form');
 form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
-    £('#submit-button').attr('disabled', true);
-    £('#payment-form').fadeToggle(100);
-    £('#loading-overlay').fadeToggle(100);
+    $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
 
     var saveInfo = Boolean($('#id-save-info').attr('checked'));
     // From using {% csrf_token %} in the form
@@ -64,33 +64,33 @@ form.addEventListener('submit', function(ev) {
     };
     var url = '/checkout/cache_checkout_data/';
 
-    £.post(url, postData).done(function () {
+    $.post(url, postData).done(function () {
         stripe.confirmCardPayment(clientSecret, {
             payment_method: {
                 card: card,
                 billing_details: {
-                    name: £.trim(form.full_name.value),
-                    phone: £.trim(form.phone_number.value),
-                    email: £.trim(form.email.value),
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
                     address:{
-                        line1: £.trim(form.street_address1.value),
-                        line2: £.trim(form.street_address2.value),
-                        city: £.trim(form.town_or_city.value),
-                        country: £.trim(form.country.value),
-                        state: £.trim(form.county.value),
+                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
+                        city: $.trim(form.town_or_city.value),
+                        country: $.trim(form.country.value),
+                        state: $.trim(form.county.value),
                     }
                 }
             },
             shipping: {
-                name: £.trim(form.full_name.value),
-                phone: £.trim(form.phone_number.value),
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
                 address: {
-                    line1: £.trim(form.street_address1.value),
-                    line2: £.trim(form.street_address2.value),
-                    city: £.trim(form.town_or_city.value),
-                    country: £.trim(form.country.value),
-                    postal_code: £.trim(form.postcode.value),
-                    state: £.trim(form.county.value),
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    postal_code: $.trim(form.postcode.value),
+                    state: $.trim(form.county.value),
                 }
             },
         }).then(function(result) {
@@ -101,11 +101,11 @@ form.addEventListener('submit', function(ev) {
                     <i class="fas fa-times"></i>
                     </span>
                     <span>${result.error.message}</span>`;
-                £(errorDiv).html(html);
-                £('#payment-form').fadeToggle(100);
-                £('#loading-overlay').fadeToggle(100);
+                $(errorDiv).html(html);
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
                 card.update({ 'disabled': false});
-                £('#submit-button').attr('disabled', false);
+                $('#submit-button').attr('disabled', false);
             } else {
                 if (result.paymentIntent.status === 'succeeded') {
                     form.submit();
